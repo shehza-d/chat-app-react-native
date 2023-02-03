e3495f001c6376900a77dd41343021fcd09d8594
Shehzad <shehzaddiqbal@gmail.com>
2023-02-03 18:42:51 +0500

    routing and structure setup

---
+++ src/styles/loginCSS.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+export const loginStyles = StyleSheet.create({
+  inputContainer: {
+    color: "yellow",
+    backgroundColor: "yellow",
+    border: "2px solid red",
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "600",
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: "400",
+  },
+  subHeadding: {
+    fontWeight: "700",
+    color: "green",
+  },
+  container: {
+    backgroundColor: "white",
+  },
+  textInput: {
+    color: "black",
+  },
+  textInput2: {},
+  labels: {},
+  labels2: {},
+  labels3: {},
+});
